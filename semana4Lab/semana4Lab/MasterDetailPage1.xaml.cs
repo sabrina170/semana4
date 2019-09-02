0c6a998962c0edd8d2a2dceb8bdd5c93ed961d6a
sabrina170 <sabrina.pomajulca@tecsup.edu.pe>
2019-09-02 18:36:21 -0500

    first commit

---
+++ semana4Lab/semana4Lab/MasterDetailPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana4Lab
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterDetailPage1 : ContentPage
+	{
+		public MasterDetailPage1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ semana4Lab/semana4Lab/ModalPage1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana4Lab
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ModalPage1 : ContentPage
+	{
+		public ModalPage1 ()
+		{
+			InitializeComponent ();
+
+            btnClosePopup.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopModalAsync();
+            };
+
+        }
+
+        
+    }
+}
